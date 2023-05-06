8e2c9af459b02eb183e85d17a43414f5eec3d232
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-05-06 18:00:14 +0300

    added user server input

--- BagelChatUnity/Assets/Scripts/Clients/Client.cs
+++ BagelChatUnity/Assets/Scripts/Clients/Client.cs
@@ -1,11 +1,14 @@
 ﻿using System.IO;
 using System.Net.Sockets;
+using BagelChat.ScriptableObjects;
 using UnityEngine;
 
 namespace BagelChat.Clients
 {
     public class Client : MonoBehaviour
     {
+        [SerializeField] private EventSO _onConnectedToServer;
+        
         private bool _socketIsReady = false;
 
         private TcpClient _socket;
@@ -14,9 +17,27 @@ namespace BagelChat.Clients
         private StreamReader _reader;
         private StreamWriter _writer;
 
+        private string _host = "127.0.0.1";
+        private string _port = "6321";
+
         public void ConnectToServer()
         {
-            
+            if (_socketIsReady)
+                return;
+
+            _onConnectedToServer.Invoke();
+        }
+
+        public void SetHost(string host)
+        {
+            _host = host;
+            Debug.Log(host);
+        }
+        
+        public void SetPort(string port)
+        {
+            _port = port;
+            Debug.Log(port);
         }
     }
 }
--- BagelChatUnity/Assets/Scripts/UI/UiManager.cs
+++ BagelChatUnity/Assets/Scripts/UI/UiManager.cs
@@ -1,19 +1,40 @@
-﻿using BagelChat.ScriptableObjects;
+﻿using BagelChat.Clients;
+using BagelChat.ScriptableObjects;
+using TMPro;
 using UnityEngine;
 
 namespace BagelChat.UI
 {
     public class UiManager : MonoBehaviour
     {
-        [SerializeField] private ScreenController[] _controllers;
         [SerializeField] private EventSO _onConnectedToServer;
+        [SerializeField] private Client _client;
 
+        [SerializeField] private ScreenController[] _controllers;
+
+        [SerializeField] private TMP_InputField _hostInput;
+        [SerializeField] private TMP_InputField _portInput;
+        
         private void OnEnable()
         {
             foreach (var controller in _controllers)
             {
                 _onConnectedToServer.OnInvoked += controller.SwitchScreen;
             }
+            
+            _hostInput.onEndEdit.AddListener(_client.SetHost);
+            _portInput.onEndEdit.AddListener(_client.SetPort);
+        }
+        
+        private void OnDisable()
+        {
+            foreach (var controller in _controllers)
+            {
+                _onConnectedToServer.OnInvoked -= controller.SwitchScreen;
+            }
+            
+            _hostInput.onEndEdit.RemoveListener(_client.SetHost);
+            _portInput.onEndEdit.RemoveListener(_client.SetPort);
         }
     }
 }
