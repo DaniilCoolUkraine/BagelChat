60c9dde7c059dbff64aec7d3325f0c667673ca85
DaniilCoolUkraine <cd0games2000@gmail.com>
2023-05-06 14:31:19 +0300

    new interaction: added base server

---
+++ BagelChatUnity/Assets/Scripts/Server/Server.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+namespace BagelChat.Server
+{
+    public class Server : MonoBehaviour
+    {
+        [SerializeField] private int _port = 6321;
+        public int Port => _port;
+
+        private List<ServerClient> _clients;
+        private List<ServerClient> _disconnectClients;
+
+        private TcpListener _server;
+        private bool _isStarted;
+
+        private void Start()
+        {
+            _clients = new List<ServerClient>();
+            _disconnectClients = new List<ServerClient>();
+
+            try
+            {
+                _server = new TcpListener(IPAddress.Any, _port);
+                _server.Start();
+
+                StartListening();
+                _isStarted = true;
+            }
+            catch (Exception e)
+            {
+                Debug.Log("Socket error " + e.Message);
+            }
+        }
+
+        private void StartListening()
+        {
+            _server.BeginAcceptTcpClient(AcceptTcpClient, _server);
+        }
+
+        private void AcceptTcpClient(IAsyncResult ar)
+        {
+            TcpListener listener = (TcpListener) ar.AsyncState;
+            
+            _clients.Add(new ServerClient(listener.EndAcceptTcpClient(ar)));
+            StartListening();
+        }
+    }
+}
---
+++ BagelChatUnity/Assets/Scripts/Server/ServerClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Sockets;
+
+namespace BagelChat.Server
+{
+    public class ServerClient
+    {
+        public ServerClient(TcpClient client)
+        {
+            Client = client;
+            Name = "guest";
+        }
+
+        public TcpClient Client { get; private set; }
+        public string Name { get; private set; }
+    }
+}
